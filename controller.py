e188d61de50f43ff961c72dcb4bb01461b8fca64
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-12-28 23:47:37 +0330

    Move subsets into utils.py

--- controller.py
+++ controller.py
@@ -5,36 +5,14 @@ from math import degrees
 
 # pyfuzzy imports
 from fuzzy.storage.fcl.Reader import Reader
-from utils import read_rules
+from utils import read_rules, load_fuzzy_sets
 
 
 class FuzzyController:
 
     def __init__(self, fcl_path):
         self.system = Reader().load_from_file(fcl_path)
-        self.fuzzy_sets = {
-            'pa': {
-                'up_more_right': [(0, 0), (30, 1), (60, 0)],
-                'up_right': [(30, 0), (60, 1), (90, 0)],
-                'up': [(60, 0), (90, 1), (120, 0)],
-                'up_left': [(90, 0), (120, 1), (150, 0)],
-                'up_more_left': [(120, 0), (150, 1), (180, 0)],
-                'down_more_left': [(180, 0), (210, 1), (240, 0)],
-                'down_left': [(210, 0), (240, 1), (270, 0)],
-                'down': [(240, 0), (270, 1), (300, 0)],
-                'down_right': [(270, 0), (300, 1), (330, 0)],
-                'down_more_right': [(300, 0), (330, 1), (360, 0)]
-            },
-
-            'pv': {
-                'cw_fast': [(-200, 1), (-100, 0)],
-                'cw_slow': [(-200, 0), (-100, 1), (0, 0)],
-                'stop': [(-100, 0), (0, 1), (100, 0)],
-                'ccw_slow': [(0, 0), (100, 1), (200, 0)],
-                'ccw_fast': [(100, 0), (200, 1)],
-            }
-        }
-
+        self.fuzzy_sets = load_fuzzy_sets()
         self.rules = read_rules()
 
     def _make_input(self, world):
